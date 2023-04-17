073aeaf626f197c0b5ccf4cbcaa9268dc1f74d38
Kasia Harai-Kamockaya <air@Kasias-Air.lan>
2023-04-17 17:08:22 -0400

    feat: add solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform( arr ) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  let doubleNext = "--double-next";
+  let discardPrev = "--discard-prev";
+  let doublePrev = "--double-prev";
+  let discardNext = "--discard-next";
+
+  let transArr = [...arr];
+  for (let i = 0; i < transArr.length; i++) {
+    if (transArr[0] === discardPrev || transArr[0] === doublePrev) {
+      transArr.splice(0, 1);
+    }
+    if (transArr[transArr.length - 1] === discardNext || transArr[transArr.length - 1] === doubleNext) {
+      transArr.splice(transArr.length - 1, 1);
+    }
+    if (transArr[i] == doubleNext) {
+      transArr.splice(i, 1, transArr[i + 1]);
+    }
+    if (transArr[i] == doublePrev) {
+      transArr.splice(i, 1, transArr[i - 1]);
+    }
+    if (transArr[i] == discardPrev) {
+      transArr.splice(i - 1, 2);
+    }
+    if (transArr[i] == discardNext) {
+      if (transArr[i + 2] == discardPrev || transArr[i + 2] == doublePrev) {
+        transArr.splice(i, 3);
+      } else {
+        transArr.splice(i, 2);
+      }
+    }
+  }
+  return transArr;
 }
 
 module.exports = {
